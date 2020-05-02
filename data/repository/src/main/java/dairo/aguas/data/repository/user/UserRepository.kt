dd01a6c9567da38b8dcc0bcbc32be4b6e2a5d26e
Dairo Aguas B <ingdairoaguas@gmail.com>
2020-05-02 11:56:09 -0500

    Repository conectado con local para guardar el usuario.

--- data/repository/src/main/java/dairo/aguas/data/repository/user/UserRepository.kt
+++ data/repository/src/main/java/dairo/aguas/data/repository/user/UserRepository.kt
@@ -1,5 +1,6 @@
 package dairo.aguas.data.repository.user
 
+import dairo.aguas.data.model.user.User
 import dairo.aguas.data.model.user.UserResponse
 import dairo.aguas.data.model.vo.Result
 
@@ -8,5 +9,7 @@ import dairo.aguas.data.model.vo.Result
  */
 interface UserRepository {
 
-    suspend fun getUserById(idUser: Int) : Result<UserResponse>
+    suspend fun getUserById(idUser: Int): Result<UserResponse>
+
+    suspend fun setUserLocal(user: User)
 }
--- data/repository/src/main/java/dairo/aguas/data/repository/user/UserRepositoryImpl.kt
+++ data/repository/src/main/java/dairo/aguas/data/repository/user/UserRepositoryImpl.kt
@@ -1,14 +1,21 @@
 package dairo.aguas.data.repository.user
 
+import dairo.aguas.data.local.dao.UserDao
+import dairo.aguas.data.model.user.User
 import dairo.aguas.data.remote.user.UserDatasource
 
 /**
  * Created by Dairo Aguas B on 2/05/2020.
  */
 class UserRepositoryImpl(
-    private val userDatasource: UserDatasource
+    private val userDatasource: UserDatasource,
+    private val userDao: UserDao
 ) : UserRepository {
 
     override suspend fun getUserById(idUser: Int) =
         userDatasource.getUserById(idUser)
+
+    override suspend fun setUserLocal(user: User) {
+        userDao.insert(user)
+    }
 }
